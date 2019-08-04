a96d7d39fd1e217ff489f4af10df14812b4066fb
Sam Flattery <samflattery@gmail.com>
2019-08-04 16:52:00 +0100

    Changed new comments from a new view controller to a table view cell

--- app/FCE++/Table View Cells/NewCommentTableViewCell.swift
+++ app/FCE++/Table View Cells/NewCommentTableViewCell.swift
@@ -7,44 +7,95 @@
 //
 
 import UIKit
+import SVProgressHUD
+import Parse
+
+protocol NewCommentCellDelegate {
+    func didPostComment(withData data: [String: Any])
+}
 
 class NewCommentTableViewCell: UITableViewCell, UITextFieldDelegate, UITextViewDelegate {
     
     @IBOutlet weak var titleField : UITextField!
     @IBOutlet weak var commentTextView : UITextView!
-
+    
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var anonymousSwitch: UISwitch!
+    @IBOutlet weak var anonymousLabel: UILabel!
+    
+    var delegate : NewCommentCellDelegate!
+    var courseNumber: String!
+    var reachability: Reachability!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        postButton.isHidden = true
+        anonymousSwitch.isHidden = true
+        anonymousLabel.isHidden = true
+        anonymousSwitch.isOn = false
+
+        // make the switch smaller
+        let switchResizeRatio: CGFloat = 0.75
+        anonymousSwitch.transform = CGAffineTransform(scaleX: switchResizeRatio, y: switchResizeRatio)
         
         commentTextView.text = "Leave your thoughts on this course or ask a question!"
         commentTextView.delegate = self
         commentTextView.textColor = .lightGray
         
         let titleBottomLine = CALayer()
-//        titleBottomLine.frame = CGRect.init(x: 0, y: titleField.frame.size.height - 1, width: titleField.frame.size.width, height: 2)
         titleBottomLine.frame = CGRect.init(x: 0, y: titleField.frame.size.height - 1, width: titleField.frame.size.width - 25, height: 1.5)
         print(titleField.frame.size.width)
         print(titleBottomLine.frame.size.width)
         print(self.frame.size.width)
         titleBottomLine.backgroundColor = UIColor.lightGray.cgColor
-//        titleBottomLine.backgroundColor = UIColor(red: 166/255, green: 25/255, blue: 46/255, alpha: 1).cgColor
-        
+
         // just have the bottom border line
         titleField.borderStyle = .none
-        titleField.layer.addSublayer(titleBottomLine)
+//        titleField.layer.addSublayer(titleBottomLine)
         titleField.delegate = self
         titleField.autocapitalizationType = .sentences
         
-        titleField.attributedPlaceholder = NSAttributedString(string: "Title", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray, NSAttributedString.Key.font: UIFont(name: "IowanOldSt OSF BT", size: 20)!])
-        
+        titleField.attributedPlaceholder = NSAttributedString(string: "Title", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray, NSAttributedString.Key.font: UIFont(name: "IowanOldSt OSF BT", size: 24)!])
     }
     
-    func textViewDidChange(_ textView: UITextView) {
-        if textView.text == "" {
-//            textView.text = "Leave your thoughts on this course or ask a question!"
-//            textView.textColor = .lightGray
+    @IBAction func postButtonPressed(_ sender: Any) {
+        if titleField.text == "" {
+            return
         }
+        
+        reachability = Reachability()!
+        
+        // if there's no internet connection, inform the user with an alert
+        if reachability.connection == .none {
+            SVProgressHUD.showError(withStatus: "No internet connection")
+            SVProgressHUD.dismiss(withDelay: 1.5)
+            return
+        }
+        
+        // get the date and time of posting in a readable format
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .medium
+        let timePosted = formatter.string(from: currentDateTime)
+        
+        let user = PFUser.current()! // the user will never be nil if this segue happens
+        
+        // format the comment data as it is in the database
+        let commentData = ["commentText": commentTextView.text!,
+                           "timePosted": timePosted,
+                           "andrewID": user.username!,
+                           "anonymous": anonymousSwitch.isOn,
+                           "header": titleField.text!,
+                           "courseNumber": courseNumber!,
+                           "replies": []] as [String : Any]
+        
+        titleField.text = ""
+        commentTextView.text = "Leave your thoughts on this course or ask a question!"
+        commentTextView.textColor = .lightGray
+        
+        delegate.didPostComment(withData: commentData)
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
@@ -64,6 +115,19 @@ class NewCommentTableViewCell: UITableViewCell, UITextFieldDelegate, UITextViewD
         }
         textView.resignFirstResponder()
     }
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        postButton.isHidden = false
+        anonymousSwitch.isHidden = false
+        anonymousLabel.isHidden = false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            postButton.isHidden = true
+            anonymousSwitch.isHidden = true
+            anonymousLabel.isHidden = true
+        }
+    }
 
 }
--- app/FCE++/View Controllers/CourseInfoTableViewController.swift
+++ app/FCE++/View Controllers/CourseInfoTableViewController.swift
@@ -13,7 +13,7 @@ import SVProgressHUD
 typealias CourseComments = [[String: Any]] // comments are stored as a list of dictionaries
 typealias CourseComment = [String: Any]
 
-class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate, NewCommentViewControllerDelegate, GuestCommentCellDelegate {
+class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate, NewCommentCellDelegate, GuestCommentCellDelegate {
 
     var query: PFQuery<PFObject>? // the currently active comment query
     var reachability: Reachability! // the user's internet status
@@ -127,12 +127,7 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "NewComment" {
-            let controller = segue.destination as! NewCommentViewController
-            controller.commentObj = commentObj
-            controller.courseNumber = course.number
-            controller.delegate = self
-        } else if segue.identifier == "ShowReplies" {
+        if segue.identifier == "ShowReplies" {
             let controller = segue.destination as! CommentRepliesViewController
             controller.commentObj = commentObj
             let commentIndex = sender as! Int
@@ -174,13 +169,13 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if segmentControl.selectedSegmentIndex == 2 {
-            if PFUser.current() == nil && indexPath.row == 0 {
+            if PFUser.current() == nil && indexPath.section == 0 {
                 // take the guest back to login screen
                 showLoginScreen()
-            } else if PFUser.current() != nil && indexPath.row == 0 {
-                performSegue(withIdentifier: "NewComment", sender: nil)
+            } else if PFUser.current() != nil && indexPath.section == 0 {
+                return
             } else {
-                performSegue(withIdentifier: "ShowReplies", sender: indexPath.row - 1)
+                performSegue(withIdentifier: "ShowReplies", sender: indexPath.row)
                 // the create new comment cell is first so it must be offset by one
             }
         }
@@ -191,6 +186,8 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
         if segmentControl.selectedSegmentIndex == 1 {
             // one segment for each instructor
             return instructorInfo.count
+        } else if segmentControl.selectedSegmentIndex == 2 {
+            return 2
         } else {
             return 1
         }
@@ -202,13 +199,14 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
         } else if segmentControl.selectedSegmentIndex == 1 {
             return 11 // one for each piece of instructor info
         } else {
-            if let comments = courseComments {
-                if isLoadingComment {
-                    return comments.count + 2 // 2 extra for new comment and loading comment cells
-                }
-                return comments.count + 1
+            if section == 0 {
+                return 1
             } else {
-                return 1 // failed to load cell
+                if let comments = courseComments {
+                    return isLoadingComment ? comments.count + 1 : comments.count
+                } else { // there were no comments to show so just show failed to load cell
+                    return 1
+                }
             }
         }
     }
@@ -216,12 +214,13 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if segmentControl.selectedSegmentIndex == 1 {
             return instructorInfo[section][0]
+        } else if segmentControl.selectedSegmentIndex == 2 && section == 0 {
+            return "New Comment"
         }
         return nil
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        var cell: UITableViewCell!
         let i = indexPath.row
         let j = indexPath.section
         
@@ -257,49 +256,49 @@ class CourseInfoTableViewController: UITableViewController, UITextFieldDelegate,
             return instructorCell
         }
         else {
-            // the comment segment's cells
-            if i == 0 && PFUser.current() != nil {
-                // show the new comment cell as the first cell
-                let newCommentCell = tableView.dequeueReusableCell(withIdentifier: "NewCommentCell", for: indexPath)
-                return newCommentCell
-//                let newCommentCell = tableView.dequeueReusableCell(withIdentifier: "CreatePost", for: indexPath)
-//                return newCommentCell
-
-            }
-            else if i == 0 && PFUser.current() == nil {
-                // if there is no user, display the cell with the login button
-                let guestCommentCell = tableView.dequeueReusableCell(withIdentifier: "GuestComment", for: indexPath) as! GuestCommentCell
-                guestCommentCell.delegate = self
-                return guestCommentCell
-            }
-            else if i == 1 && isLoadingComment {
-                // if the user posted a new comment, display the loading cell
-                let loadingCell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath) as! LoadingCell
-                loadingCell.spinner.startAnimating()
-                return loadingCell
+            if j == 0 { // the first segment is the new comment segment
+                if PFUser.current() != nil {
+                    // show the new comment cell as the first cell
+                    let newCommentCell = tableView.dequeueReusableCell(withIdentifier: "NewCommentCell", for: indexPath) as! NewCommentTableViewCell
+                    newCommentCell.delegate = self
+                    newCommentCell.courseNumber = course.number
+                    return newCommentCell
+                } else {
+                    let guestCommentCell = tableView.dequeueReusableCell(withIdentifier: "GuestComment", for: indexPath) as! GuestCommentCell
+                    guestCommentCell.delegate = self
+                    return guestCommentCell
+                }
             }
             else {
-                // display comments
-                // if the user has just posted a comment, there is a temporary cell with a loading
-                // spinner, so in this case each cell must be shifted forward by one to fit this
-                let indexRow = isLoadingComment ? indexPath.row - 2 : indexPath.row - 1
-                let commentCell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! CommentCell
-                if let commentInfo = courseComments?[indexRow] {
-                    commentCell.headerLabel.text = commentInfo["header"] as? String
-                    commentCell.commentLabel.text = commentInfo["commentText"] as? String
-                    commentCell.dateLabel.text = commentInfo["timePosted"] as? String
-                    if commentInfo["anonymous"] as! Bool {
-                        commentCell.andrewIDLabel.text = "Anonymous"
-                    } else {
-                        commentCell.andrewIDLabel.text = commentInfo["andrewID"] as? String
-                    }
+                if i == 0 && isLoadingComment {
+                    // if the user posted a new comment, display the loading cell
+                    let loadingCell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath) as! LoadingCell
+                    loadingCell.spinner.startAnimating()
+                    return loadingCell
                 }
-                return commentCell
+                else {
+                    // display comments
+                    // if the user has just posted a comment, there is a temporary cell with a loading
+                    // spinner, so in this case each cell must be shifted forward by one to fit this
+                    let indexRow = isLoadingComment ? indexPath.row - 1 : indexPath.row
+                    let commentCell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! CommentCell
+                    if let commentInfo = courseComments?[indexRow] {
+                        commentCell.headerLabel.text = commentInfo["header"] as? String
+                        commentCell.commentLabel.text = commentInfo["commentText"] as? String
+                        commentCell.dateLabel.text = commentInfo["timePosted"] as? String
+                        if commentInfo["anonymous"] as! Bool {
+                            commentCell.andrewIDLabel.text = "Anonymous"
+                        } else {
+                            commentCell.andrewIDLabel.text = commentInfo["andrewID"] as? String
+                        }
+                    }
+                    return commentCell
+                }
             }
         }
     } // end of cellForIndexAt
     
-    //MARK:- NewCommentViewControllerDelegate
+    //MARK:- NewCommentCellDelegate
     func didPostComment(withData data: [String : Any]) {
         isLoadingComment = true
         tableView.reloadData() // display loading cell
